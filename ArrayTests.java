13341e62e619b4a4346e562969054fc7ae639d91
Alex-Polito <apolito@ucsd.edu>
2023-10-23 11:28:23 -0700

    Update2 Lab4

--- ArrayTests.java
+++ ArrayTests.java
@@ -26,4 +26,10 @@ public class ArrayTests {
     int[] input1 = { 3, 4 };
     assertArrayEquals(new int[]{ 4, 3 }, ArrayExamples.reversed(input1));
 	}
+  @Test 
+	public void testSum() {
+    double[] input1 = { 1, 2, 2 };
+    double result = 2;
+    assertEquals(result, ArrayExamples.averageWithoutLowest(input1), .0001);
+	}
 }
